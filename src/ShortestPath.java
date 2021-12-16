da05e0b0dfd8ca71bec5b44ba7d53ccfa4602190
Sam Sherman <samsher4606@gmail.com>
2021-12-16 10:22:23 -0500

    Reimplemented without Pairs and fixed resolved syntax warnings

--- src/ShortestPath.java
+++ src/ShortestPath.java
@@ -1,27 +1,22 @@
-import javafx.util.Pair;
-
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class ShortestPath {
     public static Map<Character, Integer> dijkstra(Character start, AdjacencyMatrix graph){
         if(!graph.getNodes().contains(start)){
             return null;
         }
-        //import nodes into map with pair of (NodeName, Cost)
+        //import nodes into map: Node -> Cost
         Map<Character, Integer> costs = new HashMap<>();
         Set<Character> red = new HashSet<>();
-        Set<Character> useableWorkers = new HashSet<>();
+        Set<Character> usableWorkers = new HashSet<>();
         red.add(start);
-        useableWorkers.add(start);
+        usableWorkers.add(start);
         for (Character c : graph.getNodes()){
             costs.put(c, -1);
         }
         costs.replace(start, 0);
         Character working = start;
-        while(useableWorkers.size()!=0){
+        while(usableWorkers.size()!=0){
             //Now we loop through the connections that our current node has
             for (Character c : graph.getEdges(working)) {
                 //Do not bother trying to relax a completed node
@@ -30,16 +25,16 @@ public class ShortestPath {
                     if (costs.get(c) == -1 || graph.findEdge(working, c) + costs.get(working) < costs.get(c)) {
                         costs.replace(c, graph.findEdge(working, c)+ costs.get(working));
                     }
-                    useableWorkers.add(c);
+                    usableWorkers.add(c);
                 }
             }
             //Spend worker
-            useableWorkers.remove(working);
+            usableWorkers.remove(working);
             //Find unused worker with the lowest cost
-            Pair<Character, Integer> p = new Pair<>(null, -1);
-            for(Character c : useableWorkers){
+            Map.Entry<Character, Integer> p = new AbstractMap.SimpleEntry<>(null, -1);
+            for(Character c : usableWorkers){
                 if(p.getKey()==null || costs.get(c) < p.getValue()){
-                    p = new Pair<>(c, costs.get(c));
+                    p = new AbstractMap.SimpleEntry<>(c, costs.get(c));
                 }
             }
             //Set worker to the lowest
